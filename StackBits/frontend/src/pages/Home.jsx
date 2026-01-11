0e731673392f75cbae4f5328d93b6a5ff915ed22
Neelman47 <neelshah707@gmail.com>
2026-01-11 17:05:14 +0530

    backend port change

--- StackBits/frontend/src/pages/Home.jsx
+++ StackBits/frontend/src/pages/Home.jsx
@@ -2,9 +2,12 @@ import ImageUpload from "../components/ImageUpload"
 
 export default function Home() {
   return (
+    <>  
     <div className="container py-5">
+      <h2 className="text-center text-primary mb-4">StackBits Coming soon</h2>
       <h2 className="text-center mb-4">Image Compression Tool</h2>
       <ImageUpload />
     </div>
+    </>
   )
 }
